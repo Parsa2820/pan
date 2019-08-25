3457961e8aaa9e212b3ce278374b093c65134d87
Parsa2820 <39219981+Parsa2820@users.noreply.github.com>
2019-08-25 19:18:28 +0430

    Add files via upload

---
+++ s.py
@@ -0,0 +1,9 @@
+s=[1,3,5,7,9,11,13,15]
+for i1 in range (0,8):
+    for i2 in range (i1+1,8):
+        for i3 in range (i2+1,8):
+            for i4 in range (i3+1,8):
+                for i5 in range (i4+1,8):
+                    print s[i1]+s[i2]+s[i3]+s[i4]+s[i5]
+
+    
